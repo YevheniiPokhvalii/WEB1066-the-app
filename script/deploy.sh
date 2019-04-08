d59dea0775c31b8beb23e9509ab620c5a4ee7396
Edward Raigosa <wenlock@github.com>
2019-04-07 19:36:16 -0700

    adding some debugging

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-echo "${DOCKER_PASSWORD}" | docker login --username \"${DOCKER_LOGIN}\" --password-stdin
+echo "Starting deploy"
+echo "${DOCKER_PASSWORD}"|wc -c
+echo "${DOCKER_LOGIN}" | wc -c
+echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_LOGIN}" --password-stdin
 docker tag zutherb/monolithic-shop:latest $DOCKER_LOGIN/monolithic-shop:latest
 docker push $DOCKER_LOGIN/monolithic-shop:latest
